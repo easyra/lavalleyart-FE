36a9d7ed34ee3b66383823316f1ca4750d7a7fb3
ezra davis davis <ezra.rr.davis@gmail.com>
2019-06-07 16:58:51 -0700

    Logo added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
 import Navbar from './components/Navbar';
+import Logo from './components/Logo';
 
 function App() {
   return (
     <div className='App'>
+      <Logo />
       <Navbar />
     </div>
   );
---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <img
+      src='https://static.wixstatic.com/media/8c4f11_4065e715920842cb9423eba569de6c9d~mv2.gif'
+      alt='LAVA'
+      className='logo'
+    />
+  );
+};
+export default Logo;
