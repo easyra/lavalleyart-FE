366d8cd91a5a3c1a32c370529f516ee7362095e4
ezra davis davis <ezra.rr.davis@gmail.com>
2019-06-07 16:20:43 -0700

    sass add & nav bar styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <header className='navbar'>
+      <nav className='content'>
+        <ul className='nav-items'>
+          <li className='nav-item'>home</li>
+          <li className='nav-item'>calender</li>
+          <li className='nav-item'>tv</li>
+          <li className='nav-item'>music</li>
+          <li className='nav-item'>blog</li>
+          <li className='nav-item'>podcast</li>
+          <li className='nav-item'>contact us</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
