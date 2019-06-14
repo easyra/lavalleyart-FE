18995a1493ee4ad09e9c32358ab9d6496fc9a554
ezra davis davis <ezra.rr.davis@gmail.com>
2019-06-14 13:46:00 -0700

    even style finsihed

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -4,13 +4,22 @@ const Calender = () => {
     <div className='calender'>
       <div className='content'>
         <div className='current'>
-          <h2>Future Events</h2>
+          <h2>Upcoming Events</h2>
           <div className='events'>
             <div className='event'>
-              <img src='' alt='Event Name' />
+              <div className='cover-container'>
+                <img
+                  className='cover'
+                  src='https://images.pexels.com/photos/2464638/pexels-photo-2464638.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+                  alt='Event Name'
+                />
+                <div className='date'>
+                  <div className='month'>Jun</div> 13
+                </div>
+              </div>
+
               <div className='text'>
-                <h4>Event Name</h4>
-                <div className='date'>6/13/19</div>
+                <h4 className='event-name'>Event Name</h4>
                 <div className='time'>11pm-12pm</div>
               </div>
             </div>
